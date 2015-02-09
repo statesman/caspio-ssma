7d43c49356d5593527934a1b2a36a4c4e2d123c6
Christian McDonald <cmcdonald@statesman.com>
2015-02-09 09:28:56 -0600

    getting base files from another project

--- Gruntfile.js
+++ Gruntfile.js
@@ -94,7 +94,7 @@ module.exports = function(grunt) {
           authKey: 'key1'
         },
         src: 'public',
-        dest: '/stage_aas/projects/sxsw',
+        dest: '/stage_aas/projects/ssma',
         exclusions: ['dist/tmp'],
         simple: false,
         useList: false
